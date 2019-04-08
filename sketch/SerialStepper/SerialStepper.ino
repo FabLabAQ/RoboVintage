2a375d08f20676492369366639fc1945c97a6499
Luca Anastasio <anastasio.lu@gmail.com>
2019-04-08 19:21:34 +0200

    SerialStepper update

--- sketch/SerialStepper/SerialStepper.ino
+++ sketch/SerialStepper/SerialStepper.ino
@@ -1,7 +1,36 @@
+/* 
+ * This file is part of the RoboVintage https://github.com/FabLabAQ/RoboVintage.
+ * Copyright (c) 2019 FabLabAQ <info@fablaquila.org>.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/* 
+ * Example commands:
+ * 1 45 2 30 3 -30 4 -30 5 45
+ * 1 -45 2 -30 3 30 4 30 5 -45
+ * 
+ * 
+ */
+
 #include <AccelStepper.h>
 #include <MultiStepper.h>
 #include "pins.h"
 
+MultiStepper multiStepper;
+
+/*
 AccelStepper steppers[6] = {
 	AccelStepper(AccelStepper::DRIVER, StepPin1, DirPin1),
 	AccelStepper(AccelStepper::DRIVER, StepPin2, DirPin2),
@@ -10,25 +39,55 @@ AccelStepper steppers[6] = {
 	AccelStepper(AccelStepper::DRIVER, StepPin5, DirPin5),
 	AccelStepper(AccelStepper::DRIVER, StepPin6, DirPin6)
 };
+*/
+
+AccelStepper steppers[6] = {
+	AccelStepper(forward1, backward1),
+	AccelStepper(forward2, backward2),
+	AccelStepper(forward3, backward3),
+	AccelStepper(forward4, backward4),
+	AccelStepper(forward5, backward5),
+	AccelStepper(forward6, backward6)
+};
 
 long steps[6] = {0, 0, 0, 0, 0, 0};
 
+float angles[6] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+
+void MoveRelative(float * in, long * out) {
+	// base joint
+	out[0] = stepRad[0] * in[0];
+	// near arm joint
+	out[1] = stepRad[1] * in[1];
+	// far arm joint
+	out[2] = (in[2] + in[1] * 0.5) * stepRad[2];
+	// left gripper joint
+	out[3] = (in[3] - in[4] + in[1] * 0.5) * stepRad[3];
+	// right gripper joint
+	out[5] = (-in[3] - in[4] -in[1] * 0.5) * stepRad[3];
+	// gripper
+	out[4] = in[5] * DEG_TO_RAD*1000;
+}
 
 void setup() {
-	// put your setup code here, to run once:
-
+	PinSetup();
+	
 	Serial.begin(115200);
 
 	for (byte i = 0; i < 6; i++) {
-		steppers[i].setMaxSpeed(100.0);
-    	steppers[i].setAcceleration(100.0);
+		steppers[i].setMaxSpeed(250.0);
+    	//steppers[i].setAcceleration(50.0);
+    	multiStepper.addStepper(steppers[i]);
 	}
+	//steppers[5].setMaxSpeed(100.0);
 	
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 	if(Serial.available() > 1) {
+
+		do{
 		
 		int stepperNumber = Serial.parseInt() - 1;
 
@@ -39,15 +98,21 @@ void loop() {
 		}
 		else {
 		
-			steps[stepperNumber] += Serial.parseInt();
+			angles[stepperNumber] += Serial.parseFloat()*DEG_TO_RAD;
 
 			Serial.print("N. ");
 			Serial.print(stepperNumber + 1);
 			Serial.print(" S. ");
-			Serial.println(steps[stepperNumber]);
-		
-			steppers[stepperNumber].runToNewPosition(steps[stepperNumber]);
-			Serial.println("OK");
+			Serial.println(angles[stepperNumber]*RAD_TO_DEG);
 		}
+	} while (Serial.read() != '\n');
+
+			MoveRelative(angles, steps);
+			multiStepper.moveTo(steps);
+			EnableSteppers();
+			multiStepper.runSpeedToPosition();
+			DisableSteppers();
+			Serial.println("OK");
+		
 	}
 }
--- sketch/SerialStepper/pins.h
+++ sketch/SerialStepper/pins.h
@@ -1,3 +1,20 @@
+/* 
+ * This file is part of the RoboVintage https://github.com/FabLabAQ/RoboVintage.
+ * Copyright (c) 2019 FabLabAQ <info@fablaquila.org>.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef PINS_H
 #define PINS_H
 
@@ -7,8 +24,8 @@
  *  -----------------------------
  * |     stepsticks      | power |
  * |---------------------|-------|
- * | 4:       | 1:       |  +12V |
- * |   stp 12 |   stp 11 |  -GND |
+ * | 4:       | 1:       |  12V  |
+ * |   stp 12 |   stp 11 |  GND  |
  * |   dir 13 |   dir 10 |-------
  * |----------|----------|
  * | 5:       | 2:       |
@@ -21,31 +38,26 @@
  * |---------------------|
  * |endstops/gen. purpose|
  * |---------------------|
- * |A0 A1 A2 A3 A4 A5 A6 |
+ * |GND GND GND A4 GND 5V|
+ * |A0  A1  A2  A3 GND 5V|
  *  ---------------------
  *  
  *  Motor description:
  *  1: Base joint
- *  2: 
- *  3:
- *  4:
- *  5:
- *  6:
+ *  2: Near arm joint
+ *  3: Far arm joint
+ *  4: Left gripper joint
+ *  5: Gripper
+ *  6: Right gripper joint
  *  
  *  Endstop description:
- *  A6: gripper
+ *  A0: Gripper
+ *  A1: Near arm
+ *  A2: Far arm
+ *  A4: Base
+ *  A3: spare
  *  
- */
-
-/*  Steps/deg:
- *  Base joint = 8.333
- *  Lower arm = 8.333
- *  
- *  
- *  Limits:
- *  Base joint = -150/+150
- *  
- *  
+ *  Steppers enable: A5
  *  
  */
 
@@ -61,7 +73,172 @@ const uint8_t
 	DirPin5 = 9,
 	StepPin5 = 8,
 	DirPin6 = 5,
-	StepPin6 = 4;
+	StepPin6 = 4,
+	EnPin = A5,
+	EndPin1 = A4,
+	EndPin2 = A1,
+	EndPin3 = A2,
+	EndPin4 = A3,
+	EndPin5 = A0;
+
+#define DIR_DELAY 1
+#define STEP_DELAY 2
+
+#define DELAY_CYCLES(n) __builtin_avr_delay_cycles(n)
+
+#define	 DIR_PIN_1 PB2
+#define	STEP_PIN_1 PB3
+#define	 DIR_PIN_2 PD6
+#define	STEP_PIN_2 PD7
+#define	 DIR_PIN_3 PD2
+#define	STEP_PIN_3 PD3
+#define	 DIR_PIN_4 PB5
+#define	STEP_PIN_4 PB4
+#define	 DIR_PIN_5 PB1
+#define	STEP_PIN_5 PB0
+#define	 DIR_PIN_6 PD5
+#define	STEP_PIN_6 PD4
+
+const int stepRad[] = {
+	480,
+	480,
+	500,
+	600
+};
+
+const float limits[][2] = {
+	{-2.618, 2.618},
+	{-2.356, 0.785},
+	{-2.356, -0.09},
+	{-1.57, 1.57},
+	{0, 95}
+};
+
+const float ArmJointComp = 2.0;
+
+void EnableSteppers() {
+	PORTC = 0x1F;
+}
+
+void DisableSteppers() {
+	PORTC = 0x3F;
+}
+
+void PinSetup() {
+	/*
+	pinMode(EnPin, OUTPUT);
+	digitalWrite(EnPin, HIGH);
+	pinMode(EndPin1, INPUT_PULLUP);
+	pinMode(EndPin2, INPUT_PULLUP);
+	pinMode(EndPin3, INPUT_PULLUP);
+	pinMode(EndPin4, INPUT_PULLUP);
+	pinMode(EndPin5, INPUT_PULLUP);
+	*/
+	// equivalent to:
+	PORTC = 0x3F;
+	DDRC = _BV(PC5);
+	
+	// the following is equivalent to
+	// pinMode(every DirPinX/StepPinY, OUTPUT)
+	DDRB = 0x3F;
+	DDRD = 0xFC;
+}
+
+inline void forward1 () {
+	PORTB |= _BV(DIR_PIN_1);
+	DELAY_CYCLES(DIR_DELAY);
+	PORTB |= _BV(STEP_PIN_1);
+	DELAY_CYCLES(STEP_DELAY);
+	PORTB &= ~_BV(STEP_PIN_1);
+}
+
+inline void backward1 () {
+	PORTB &= ~_BV(DIR_PIN_1);
+	DELAY_CYCLES(DIR_DELAY);
+	PORTB |= _BV(STEP_PIN_1);
+	DELAY_CYCLES(STEP_DELAY);
+	PORTB &= ~_BV(STEP_PIN_1);
+}
+
+inline void forward4 () {
+	PORTB |= _BV(DIR_PIN_4);
+	DELAY_CYCLES(DIR_DELAY);
+	PORTB |= _BV(STEP_PIN_4);
+	DELAY_CYCLES(STEP_DELAY);
+	PORTB &= ~_BV(STEP_PIN_4);
+}
+
+inline void backward4 () {
+	PORTB &= ~_BV(DIR_PIN_4);
+	DELAY_CYCLES(DIR_DELAY);
+	PORTB |= _BV(STEP_PIN_4);
+	DELAY_CYCLES(STEP_DELAY);
+	PORTB &= ~_BV(STEP_PIN_4);
+}
+
+inline void forward5 () {
+	PORTB |= _BV(DIR_PIN_5);
+	DELAY_CYCLES(DIR_DELAY);
+	PORTB |= _BV(STEP_PIN_5);
+	DELAY_CYCLES(STEP_DELAY);
+	PORTB &= ~_BV(STEP_PIN_5);
+}
+
+inline void backward5 () {
+	PORTB &= ~_BV(DIR_PIN_5);
+	DELAY_CYCLES(DIR_DELAY);
+	PORTB |= _BV(STEP_PIN_5);
+	DELAY_CYCLES(STEP_DELAY);
+	PORTB &= ~_BV(STEP_PIN_5);
+}
+
+inline void forward2 () {
+	PORTD |= _BV(DIR_PIN_2);
+	DELAY_CYCLES(DIR_DELAY);
+	PORTD |= _BV(STEP_PIN_2);
+	DELAY_CYCLES(STEP_DELAY);
+	PORTD &= ~_BV(STEP_PIN_2);
+}
+
+inline void backward2 () {
+	PORTD &= ~_BV(DIR_PIN_2);
+	DELAY_CYCLES(DIR_DELAY);
+	PORTD |= _BV(STEP_PIN_2);
+	DELAY_CYCLES(STEP_DELAY);
+	PORTD &= ~_BV(STEP_PIN_2);
+}
+
+inline void forward3 () {
+	PORTD |= _BV(DIR_PIN_3);
+	DELAY_CYCLES(DIR_DELAY);
+	PORTD |= _BV(STEP_PIN_3);
+	DELAY_CYCLES(STEP_DELAY);
+	PORTD &= ~_BV(STEP_PIN_3);
+}
+
+inline void backward3 () {
+	PORTD &= ~_BV(DIR_PIN_3);
+	DELAY_CYCLES(DIR_DELAY);
+	PORTD |= _BV(STEP_PIN_3);
+	DELAY_CYCLES(STEP_DELAY);
+	PORTD &= ~_BV(STEP_PIN_3);
+}
+
+inline void forward6 () {
+	PORTD |= _BV(DIR_PIN_6);
+	DELAY_CYCLES(DIR_DELAY);
+	PORTD |= _BV(STEP_PIN_6);
+	DELAY_CYCLES(STEP_DELAY);
+	PORTD &= ~_BV(STEP_PIN_6);
+}
+
+inline void backward6 () {
+	PORTD &= ~_BV(DIR_PIN_6);
+	DELAY_CYCLES(DIR_DELAY);
+	PORTD |= _BV(STEP_PIN_6);
+	DELAY_CYCLES(STEP_DELAY);
+	PORTD &= ~_BV(STEP_PIN_6);
+}
 
 
 
